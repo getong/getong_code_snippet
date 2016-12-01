5ae34f092c2affcf4d61c532817c8223f1f3bf1c
19年梦醒 <3949379+getong@users.noreply.github.com>
2016-12-01 22:26:49 +0800

    add emacs init code

---
+++ emacs_code/init.el
@@ -0,0 +1,235 @@
+;;设定光标为短线
+(setq-default cursor-type 'bar)
+
+;; ispell 中文问题
+;; use apsell as ispell backend
+(setq-default ispell-program-name "aspell")
+;; use American English as ispell default dictionary
+(ispell-change-dictionary "american" t)
+
+;;以tab缩进
+(setq indent-tabs-mode t)
+
+;;参考 http://www.aiuxian.com/article/p-823990.html
+;; Linux下emacs如何和X-Window系统共享剪贴板
+(setq x-select-enable-clipboard t)
+
+;; use xsel to copy/paste in emacs-nox
+(unless window-system
+  (when (getenv "DISPLAY")
+    (defun xsel-cut-function (text &optional push)
+      (with-temp-buffer
+        (insert text)
+        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    (defun xsel-paste-function()
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (unless (string= (car kill-ring) xsel-output)
+          xsel-output )))
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    ))
+
+
+(global-superword-mode t)
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "blue")
+(set-face-foreground 'highlight nil)
+
+(blink-cursor-mode 0)
+(set-cursor-color "blue")
+(global-font-lock-mode t)
+
+(global-linum-mode 1)
+(setq linum-format "%3d ")
+(add-hook 'prog-mode-hook 'linum-mode)
+
+;;(setq-default indent-tabs-mode t)
+(ido-mode 1)
+(setq column-number-mode t)
+
+(setq inhibit-startup-message t) ;; 关闭起动时LOGO
+(setq visible-bell t);;关闭出错时的提示声
+(global-font-lock-mode t);语法高亮
+(show-paren-mode t) ;; 显示括号匹配
+(setq show-paren-style 'parenthesis)
+(setq mouse-yank-at-point t);;支持中键粘贴
+(mouse-avoidance-mode 'animate) ;;光标靠近鼠标指针时，让鼠标指针自动让开，别挡住视线。
+(setq appt-issue-message t)
+
+(add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
+(add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
+
+
+(setq backup-directory-alist (quote (("." . "~/.backups"))))
+(setq version-control t)
+(setq kept-old-versions 2)
+(setq kept-new-versions 5)
+(setq delete-old-versions t)
+(setq backup-directory-alist '(("." . "~/.backups")))
+(setq backup-by-copying t)
+
+(global-auto-revert-mode 1)
+
+;; Behave like vi's o command
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+  See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'open-next-line)
+
+;; Behave like vi's O command
+(defun open-previous-line (arg)
+  "Open a new line before the current one.
+  See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "M-o") 'open-previous-line)
+
+;; Autoindent open-*-lines
+(defvar newline-and-indent t
+  "Modify the behavior of the open-*-line functions to cause them to autoindent.")
+
+(setq initial-scratch-message nil)
+
+(global-unset-key (kbd "C-SPC")) ;; 输入法快捷键冲突
+(global-set-key (kbd "M-SPC") 'set-mark-command)
+
+(icomplete-mode 99)
+
+(defun split-window-4()
+  "Splite window into 4 sub-window"
+  (interactive)
+  (if (= 1 (length (window-list)))
+      (progn (split-window-vertically)
+             (split-window-horizontally)
+             (other-window 2)
+             (split-window-horizontally)
+             )
+    )
+  )
+
+(defun change-split-type (split-fn &optional arg)
+  "Change 3 window style from horizontal to vertical and vice-versa"
+  (let ((bufList (mapcar 'window-buffer (window-list))))
+    (select-window (get-largest-window))
+    (funcall split-fn arg)
+    (mapcar* 'set-window-buffer (window-list) bufList)))
+
+
+(defun change-split-type-2 (&optional arg)
+  "Changes splitting from vertical to horizontal and vice-versa"
+  (interactive "P")
+  (let ((split-type (lambda (&optional arg)
+                      (delete-other-windows-internal)
+                      (if arg (split-window-vertically)
+                        (split-window-horizontally)))))
+    (change-split-type split-type arg)))
+
+
+(defun change-split-type-3-v (&optional arg)
+  "change 3 window style from horizon to vertical"
+  (interactive "P")
+  (change-split-type 'split-window-3-horizontally arg))
+
+(defun change-split-type-3-h (&optional arg)
+  "change 3 window style from vertical to horizon"
+  (interactive "P")
+  (change-split-type 'split-window-3-vertically arg))
+
+(defun split-window-3-horizontally (&optional arg)
+  "Split window into 3 while largest one is in horizon"
+                                        ;  (interactive "P")
+  (delete-other-windows)
+  (split-window-horizontally)
+  (if arg (other-window 1))
+  (split-window-vertically))
+
+(defun split-window-3-vertically (&optional arg)
+  "Split window into 3 while largest one is in vertical"
+  ;; (interactive "P")
+  (delete-other-windows)
+  (split-window-vertically)
+  (if arg (other-window 1))
+  (split-window-horizontally))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'pangu-spacing)
+(global-pangu-spacing-mode 1)
+
+(spinner-start 'vertical-breathing 10)
+(spinner-start 'minibox)
+(spinner-start 'moon)
+(spinner-start 'triangle)
+
+;; 高亮显示选中区域
+(transient-mark-mode t)
+;; 高亮选中区域颜色
+(set-face-attribute 'region nil :background "#666" :foreground "#ffffff")
+
+(indent-guide-global-mode)
+
+(defun switch-to-frame (frame-name)
+  (interactive "sFrame name:")
+  (let ((frames (frame-list)))
+    (catch 'break
+      (while frames (let ((frame (car frames)))
+                      (if (equal (frame-parameter frame 'name) frame-name)
+                          (throw 'break (select-frame-set-input-focus frame))
+                        (setq frames (cdr frames))))))))
+
+(global-set-key [f9] 'toggle-menu-bar-mode-from-frame)
+
+;; 关闭所有的buffer
+(defun close-all-buffers ()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+
+(setq x-underline-at-descent-line t)
+
+
+(add-hook 'after-init-hook 'my-after-init-hook)
+(defun my-after-init-hook ()
+  (require 'edts-start))
+
+
+(setq erlang-root-dir "/usr/local/otp_src_19.1.6/lib/erlang")
+(setq erlang-man-root "/usr/local/otp_src_19.1.6/lib/erlang")
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 开启全局 Company 补全
+(global-company-mode 1)
+
+;;文本解码设置默认为 UTF-8
+(set-language-environment "UTF-8")
+
+;; Emacs 自动加载外部修改过的文件
+(global-auto-revert-mode 1)
+
+(setq company-idle-delay 0.08)
+(setq company-minimum-prefix-length 1)
+
+;; set default tab char's display width to 4 spaces
+(setq-default tab-width 4) ; emacs 23.1, 24.2, default to 8
+
+;; 对齐插入空格而不是tab
+;; copy from http://stackoverflow.com/questions/22710040/emacs-align-regexp-with-spaces-instead-of-tabs
+(defadvice align-regexp (around align-regexp-with-spaces activate)
+  (let ((indent-tabs-mode nil))
+    ad-do-it))
+
+;;如果有喜欢用鼠标选择emacs文本的同学, 可以试试加上这句配置:
+(setq mouse-drag-copy-region t)
