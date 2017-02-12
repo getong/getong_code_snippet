adc9113065b2b457e9b54fabb4b65020cf041d29
19年梦醒 <3949379+getong@users.noreply.github.com>
2017-02-12 23:46:19 +0800

    add struct example

---
+++ elixir_code/struct.ex
@@ -0,0 +1,17 @@
+defmodule Db do
+
+  defstruct [
+    name: nil,
+    style: nil,
+    color: nil,
+    opts: %{}
+  ]
+
+end
+
+defmodule Db2 do
+
+  defstruct name: nil,
+    style: nil, color: nil, opts: %{}
+
+end
