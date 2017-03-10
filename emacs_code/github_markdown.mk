7e322d8b627a01a33edfd22639e3ab1efbf238e9
19年梦醒 <3949379+getong@users.noreply.github.com>
2017-03-10 10:45:38 +0800

    add online editor

--- emacs_code/github_markdown.mk
+++ emacs_code/github_markdown.mk
@@ -1,2 +1,9 @@
-#gihub markdown reference
+#markdown
+
+## gihub markdown reference
 [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
+
+## markdown online editor
+[StackEdit](https://stackedit.io/editor)
+[stackedit on github](https://github.com/benweet/stackedit)
+[Dillinger](http://dillinger.io/)
