6f4dbc72920846036416c7c00bf6b6ca35c38160
19年梦醒 <3949379+getong@users.noreply.github.com>
2019-07-29 20:43:57 +0800

    update for debian 10

--- debian_code/download.sh
+++ debian_code/download.sh
@@ -1,81 +1,96 @@
 #!/bin/sh
-wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-1.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-2.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-3.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-4.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-5.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-6.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-7.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-8.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-9.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-10.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-11.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-12.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-13.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-14.jigdo  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-1.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-2.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-3.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-4.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-5.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-6.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-7.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-8.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-9.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-10.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-11.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-12.jigdo ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-1.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-2.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-3.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-4.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-5.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-6.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-7.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-8.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-9.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-10.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-11.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-12.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-13.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-14.template  ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-1.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-2.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-3.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-4.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-5.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-6.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-7.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-8.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-9.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-10.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-11.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-12.template ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/source/iso-dvd/SHA256SUMS -O debian-9.6.0-source.sha256sums ; \
-	wget -c http://cdimage.debian.org/debian-cd/9.6.0/amd64/iso-dvd/SHA256SUMS -O debian-9.6.0-amd64.sha256sums ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-1.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-2.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-3.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-4.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-5.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-6.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-7.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-8.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-9.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-10.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-11.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-12.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-13.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/amd64/jigdo-dvd/debian-9.6.0-amd64-DVD-14.jigdo  ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-1.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-2.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-3.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-4.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-5.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-6.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-7.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-8.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-9.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-10.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-11.jigdo ; \
-	jigdo-lite --noask http://cdimage.debian.org/debian-cd/9.6.0/source/jigdo-dvd/debian-9.6.0-source-DVD-12.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-1.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-2.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-3.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-4.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-5.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-6.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-7.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-8.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-9.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-10.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-11.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-12.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-13.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-14.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-15.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-16.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-1.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-2.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-3.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-4.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-5.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-6.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-7.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-8.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-9.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-10.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-11.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-12.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-13.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-14.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-15.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-16.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/SHA256SUMS -O debian-amd64-sha256sum.txt
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-1.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-2.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-3.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-4.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-5.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-6.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-7.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-8.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-9.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-10.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-11.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-12.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-13.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-14.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-15.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/amd64/jigdo-dvd/debian-10.0.0-amd64-DVD-16.jigdo
+
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-1.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-2.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-3.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-4.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-5.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-6.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-7.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-8.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-9.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-10.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-11.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-12.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-13.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-14.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-15.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-16.jigdo
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-1.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-2.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-3.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-4.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-5.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-6.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-7.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-8.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-9.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-10.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-11.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-12.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-13.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-14.template
+wget -c http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/SHA256SUMS -O debian-source-sha256sum.txt
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-1.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-2.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-3.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-4.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-5.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-6.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-7.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-8.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-9.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-10.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-11.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-12.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-13.jigdo
+jigdo-lite --noask http://mirrors.163.com/debian-cd/10.0.0/source/jigdo-dvd/debian-10.0.0-source-DVD-14.jigdo
