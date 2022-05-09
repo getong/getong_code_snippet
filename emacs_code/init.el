494fbf1310d87a10d60f2a7bb5cda1b52a06ff02
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-05-09 18:07:30 +0800

    Aligning columns in Emacs

--- emacs_code/init.el
+++ emacs_code/init.el
@@ -553,3 +553,9 @@
 (advice-add 'undo-tree-visualize-redo-to-x :after #'undo-tree-visualizer-update-linum)
 (advice-add 'undo-tree-visualizer-mouse-set :after #'undo-tree-visualizer-update-linum)
 (advice-add 'undo-tree-visualizer-set :after #'undo-tree-visualizer-update-linum)
+
+;; copy from [Aligning columns in Emacs](https://blog.lambda.cx/posts/emacs-align-columns/)
+(defun align-non-space (BEG END)
+  "Align non-space columns in region BEG END."
+  (interactive "r")
+  (align-regexp BEG END "\\(\\s-*\\)\\S-+" 1 1 t))
