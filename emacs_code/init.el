a3d6238b6673ee3d2757da87fe49c9bdfd1ff4ba
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-06-05 09:24:27 +0800

    add swift mode

--- emacs_code/init.el
+++ emacs_code/init.el
@@ -1,6 +1,6 @@
 ; copy from [How to automatically install Emacs packages by specifying a list of package names?](https://stackoverflow.com/questions/10092322/how-to-automatically-install-emacs-packages-by-specifying-a-list-of-package-name)
 ; list the packages you want
-(setq package-list '(edts company indent-guide pangu-spacing spinner undo-tree highlight-thing markdown-mode switch-window protobuf-mode elixir-mode alchemist tide dart-mode dart-server mix csharp-mode omnisharp lua-mode flycheck-rust rust-mode use-package))
+(setq package-list '(edts company indent-guide pangu-spacing spinner undo-tree highlight-thing markdown-mode switch-window protobuf-mode elixir-mode alchemist tide dart-mode dart-server mix csharp-mode omnisharp lua-mode flycheck-rust rust-mode swift-mode use-package))
 
 ; list the repositories containing them
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
@@ -580,3 +580,63 @@ Get it from:  <http://hasseg.org/trash/>"
                            "| sed -e 's/^/Trashed: /'")
                    nil ;; Name of output buffer
                    "*Trash Error Buffer*")))
+
+;; copy from https://www.danielde.dev/blog/emacs-for-swift-development
+(defun print-swift-var-under-point()
+    (interactive)
+    (if (string-match-p (string (preceding-char)) "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+        (backward-sexp)
+      nil)
+    (kill-sexp)
+    (yank)
+    (move-end-of-line nil)
+    (newline)
+    (insert "print(\"")
+    (yank)
+    (insert ": \\(")
+    (yank)
+    (insert ")\")")
+    (indent-for-tab-command))
+(use-package swift-mode
+  :bind (("C-c l" . print-swift-var-under-point)))
+
+(defun xcode-build()
+  (interactive)
+  (shell-command-to-string
+     "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'build targetProject' -e 'end tell'"))
+(defun xcode-run()
+  (interactive)
+  (shell-command-to-string
+     "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'stop targetProject' -e 'run targetProject' -e 'end tell'"))
+(defun xcode-test()
+  (interactive)
+  (shell-command-to-string
+     "osascript -e 'tell application \"Xcode\"' -e 'set targetProject to active workspace document' -e 'stop targetProject' -e 'test targetProject' -e 'end tell'"))
+(global-set-key (kbd "C-c p b") 'xcode-build)
+(global-set-key (kbd "C-c p r") 'xcode-run)
+(global-set-key (kbd "C-c p t") 'xcode-test)
+
+(defun xcode-open-current-file()
+  (interactive)
+  (shell-command-to-string
+    (concat "open -a \"/Applications/Xcode.app\" " (buffer-file-name)))
+  (kill-new (car (cdr (split-string (what-line)))))
+  (shell-command-to-string
+     "open keysmith://run-shortcut/796BB627-5433-48E4-BB54-1AA6C54A14E8"))
+(global-set-key (kbd "C-c p o") 'xcode-open-current-file)
+
+(defun insert-todo-comment ()
+  (interactive)
+  (indent-for-tab-command)
+  (insert "TODO: ")
+  (back-to-indentation)
+  (set-mark-command nil)
+  (move-end-of-line nil)
+  (comment-dwim nil))
+(defun todo-comment-on-next-line ()
+  "Insert a TODO comment on the next line at the proper indentation"
+  (interactive)
+  (move-end-of-line nil)
+  (newline)
+  (insert-todo-comment))
+(add-hook 'prog-mode-hook (lambda () (local-set-key (kbd "") #'todo-comment-on-next-line)))
